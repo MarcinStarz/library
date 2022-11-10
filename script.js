9b5b87574cb55b0e783410ca3835e0d07b3e6174
Marcin <marcinstarzomski@gmail.com>
2022-11-10 15:31:59 +0100

    Add basic add book function

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
+const form = document.querySelector('form');
+const addBookBtn = document.querySelector('.addbook-btn');
+
+form.addEventListener('submit', (e) => 
+    e.preventDefault());
+addBookBtn.addEventListener('click', () => addBookToLibrary());
+
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-}
+};
 
 Book.prototype.info = function() {
-    return this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read
-}
+    return this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read;
+};
+
+function addBookToLibrary() {
+    const ftitle = document.getElementById('title').value;
+    const fauthor = document.getElementById('author').value;
+    const fpages = document.getElementById('pages').value;
+    const fread = document.getElementById('is-read');
+    
+    const newBook = new Book(ftitle, fauthor, fpages, fread);
+    myLibrary.push(newBook);
+    console.log(newBook);
+    console.log(myLibrary);
+};
